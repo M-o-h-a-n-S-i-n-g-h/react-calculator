d9ad5938eb7862ce979adaae7edd2794dd01d40b
M-o-h-a-n-S-i-n-g-h <mohan.singh1385664@gmail.com>
2022-02-13 15:44:05 +0530

    Fixed indentaions

--- src/components/CalculatorButtons/CalculatorContainer.jsx
+++ src/components/CalculatorButtons/CalculatorContainer.jsx
@@ -3,11 +3,19 @@ import styles from "./calculatorButtons.module.css";
 import CalculatorButton from "./CaculatorButton";
 
 const numbers = [
-	"1", "2", "3",
-	"4", "5", "6",
-	"7", "8", "9",
-	"Clear", "0", "="
-]
+	"1",
+	"2",
+	"3",
+	"4",
+	"5",
+	"6",
+	"7",
+	"8",
+	"9",
+	"Clear",
+	"0",
+	"=",
+];
 const operators = ["+", "-", "*", "/"];
 const scientificModeOperators = ["(+/-)", "x²", "√"];
 
@@ -18,82 +26,85 @@ const initialState = {
 	result: "0",
 	isEquals: false,
 	scientificMode: false,
-	selectedScientificOperation: null
-}
+	selectedScientificOperation: null,
+};
 
 const CalculatorContainer = () => {
 	const [calculatorState, setCalculatorState] = useState(initialState);
-	
+
 	const {
 		previousNumber,
 		currentNumber,
 		selectedOperation,
 		result,
 		scientificMode,
-		selectedScientificOperation
+		selectedScientificOperation,
 	} = calculatorState;
-	
+
 	useEffect(() => {
-		console.log("came")
-		calculateScientificOperators()
-	}, [selectedScientificOperation])
-	
+		console.log("came");
+		calculateScientificOperators();
+	}, [selectedScientificOperation]);
+
 	const numbersHandler = ({ target: { value } }) => {
 		setCalculatorState(({ currentNumber }) => {
 			return {
 				...calculatorState,
-				currentNumber: (currentNumber + value),
-				result: (currentNumber + value),
-			}
-		})
-	}
-	
+				currentNumber: currentNumber + value,
+				result: currentNumber + value,
+			};
+		});
+	};
+
 	const operatorsHandler = ({ target: { value } }) => {
 		if (!currentNumber && !previousNumber) return calculatorState;
-		
+
 		if (previousNumber && !currentNumber && selectedOperation) {
 			setCalculatorState({
 				...calculatorState,
-				selectedOperation: value
-			})
+				selectedOperation: value,
+			});
 			return;
 		}
-		
+
 		if (!previousNumber) {
 			setCalculatorState({
 				...calculatorState,
 				previousNumber: currentNumber,
 				currentNumber: "",
 				selectedOperation: value,
-			})
+			});
 		} else {
 			setCalculatorState({
 				...calculatorState,
 				previousNumber: calculate(),
 				result: calculate(),
 				currentNumber: "",
-				selectedOperation: value
-			})
+				selectedOperation: value,
+			});
 		}
-	}
-	
+	};
+
 	const scientificOperatorsHandler = ({ target: { value } }) => {
-		setCalculatorState({ ...calculatorState, selectedScientificOperation: value });
+		setCalculatorState({
+			...calculatorState,
+			selectedScientificOperation: value,
+		});
 		calculateScientificOperators();
-	}
-	
+	};
+
 	const setInitialState = () => {
-		setCalculatorState(initialState)
-	}
-	
+		setCalculatorState(initialState);
+	};
+
 	const clearAll = () => {
 		setInitialState();
-	}
-	
+	};
+
 	const equals = () => {
 		// Checking if every values are available to calculate
 		if (!previousNumber || !currentNumber || !selectedOperation) return;
-		
+
 		// If every values are available
 		setCalculatorState({
 			...calculatorState,
@@ -101,66 +112,71 @@ const CalculatorContainer = () => {
 			currentNumber: "",
 			result: calculate(),
 			previousNumber: "",
-			isEquals: true
-		})
-	}
-	
+			isEquals: true,
+		});
+	};
+
 	const computeNumbers = (formula) => {
-		const isInvert = (formula === "invert");
-		const isSquare = (formula === "square");
-		const isSquareRoot = (formula === "squareRoot");
-		
+		const isInvert = formula === "invert";
+		const isSquare = formula === "square";
+		const isSquareRoot = formula === "squareRoot";
+
 		const getResult = () => {
 			if (isInvert) {
-				return (currentNumber * -1)
+				return currentNumber * -1;
 			}
 			if (isSquare) {
-				return (currentNumber * currentNumber)
+				return currentNumber * currentNumber;
 			}
 			if (isSquareRoot) {
-				return Math.sqrt(parseInt(currentNumber))
+				return Math.sqrt(parseInt(currentNumber));
 			}
-		}
-		
+		};
+
 		if (!previousNumber) {
 			setCalculatorState({
 				...calculatorState,
 				result: getResult().toString(),
 				currentNumber: getResult().toString(),
-			})
+			});
 		} else {
 			setCalculatorState(({ currentNumber }) => {
 				return {
 					...calculatorState,
-					currentNumber: (previousNumber && currentNumber) ? getResult().toString() : currentNumber,
-					result: (previousNumber && currentNumber) ? getResult().toString() : currentNumber,
-				}
-			})
+					currentNumber:
+						previousNumber && currentNumber
+							? getResult().toString()
+							: currentNumber,
+					result:
+						previousNumber && currentNumber
+							? getResult().toString()
+							: currentNumber,
+				};
+			});
 		}
-	}
-	
-	
+	};
+
 	const calculateScientificOperators = () => {
 		switch (selectedScientificOperation) {
 			case "(+/-)":
 				computeNumbers("invert");
 				break;
 			case "x²":
-				computeNumbers("square")
+				computeNumbers("square");
 				break;
 			case "√":
-				computeNumbers("squareRoot")
+				computeNumbers("squareRoot");
 				break;
 			default:
 				return calculatorState;
 		}
-	}
-	
+	};
+
 	const calculate = () => {
 		let output;
 		const firstOperand = parseInt(previousNumber);
 		const secondOperand = parseInt(currentNumber);
-		
+
 		switch (selectedOperation) {
 			case "+":
 				output = firstOperand + secondOperand;
@@ -178,28 +194,34 @@ const CalculatorContainer = () => {
 				output = "0";
 				break;
 		}
-		
+
 		return output;
-	}
-	
+	};
+
 	const getPlaceHolder = (button) => {
 		switch (button) {
 			case "+":
-				return "Add (+)"
+				return "Add (+)";
 			case "-":
-				return "Subtract (-)"
+				return "Subtract (-)";
 			case "*":
-				return "Multiply (*)"
+				return "Multiply (*)";
 			case "/":
-				return "Divide (/)"
+				return "Divide (/)";
 			default:
-				return button
+				return button;
 		}
-	}
-	
+	};
+
 	return (
 		<div>
-			<h3 style={{ display: "flex", justifyContent: "flex-end", fontSize: "44px" }}>
+			<h3
+				style={{
+					display: "flex",
+					justifyContent: "flex-end",
+					fontSize: "44px",
+				}}
+			>
 				{result || "0"}
 			</h3>
 			<div style={{ display: "flex" }}>
@@ -208,7 +230,13 @@ const CalculatorContainer = () => {
 						<CalculatorButton
 							key={index}
 							value={button}
-							onClick={(button === "Clear") ? clearAll : (button === "=") ? equals : numbersHandler}
+							onClick={
+								button === "Clear"
+									? clearAll
+									: button === "="
+									? equals
+									: numbersHandler
+							}
 						>
 							{button}
 						</CalculatorButton>
@@ -228,11 +256,16 @@ const CalculatorContainer = () => {
 				</div>
 			</div>
 			<button
-				onClick={() => setCalculatorState({ ...calculatorState, scientificMode: !scientificMode })}
+				onClick={() =>
+					setCalculatorState({
+						...calculatorState,
+						scientificMode: !scientificMode,
+					})
+				}
 			>
 				On Scientific Mode
 			</button>
-			{(
+			{
 				<div className="scientificButtons">
 					{scientificModeOperators.map((operator, index) => (
 						<CalculatorButton
@@ -245,9 +278,9 @@ const CalculatorContainer = () => {
 						</CalculatorButton>
 					))}
 				</div>
-			)}
+			}
 		</div>
-	)
-}
+	);
+};
 
 export default CalculatorContainer;
